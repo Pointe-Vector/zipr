e7541500ee8487995338be696763558b133dfbee
Andrew Hoekstra <andrew@pointevector.com>
2022-02-07 12:30:33 -0500

    Add author and url data to distribution. Add core as dependency for plugins.

--- zipr-azure/setup.py
+++ zipr-azure/setup.py
@@ -2,10 +2,13 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='zipr-azure',
-    version='0.0.1',
+    version='0.0.2',
+    author='Andrew Hoekstra',
+    author_email='andrew@pointevector.com',
+    url='https://github.com/Pointe-Vector/zipr',
     packages=find_namespace_packages(include=['zipr.*']),
-    # install_requires=[
-    #     'zipr-core',
-    # ],
+    install_requires=[
+        'zipr-core',
+    ],
     entry_points={'zipr.plugins': 'Azure = zipr.azure'},
 )
