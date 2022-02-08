9d9a8edd1102fcdbe6aa00a37d1256896cbf4942
Andrew Hoekstra <andrew@pointevector.com>
2022-02-07 21:38:47 -0500

    Bump version

--- zipr-http/setup.py
+++ zipr-http/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='zipr-http',
-    version='0.0.3',
+    version='0.0.4',
     author='Andrew Hoekstra',
     author_email='andrew@pointevector.com',
     url='https://github.com/Pointe-Vector/zipr',
