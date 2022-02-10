ce57422071931a3136ac6ec872dbb1026a7cf0f6
Andrew Hoekstra <andrew@pointevector.com>
2022-02-10 11:20:16 -0500

    Use struct to parse bytes rather than python slicing. Fix generator for central directory records.

--- zipr-core/zipr/core/zip.py
+++ zipr-core/zipr/core/zip.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, fields
 import inspect
+import struct
 
 @dataclass(frozen=True)
 class EOCD:
@@ -9,8 +10,8 @@ class EOCD:
     cd_records_total: int
     cd_size: int
     cd_offset: int
-    comment_len: int=0
-    comment: str=''
+    comment_len: int
+    comment: str
 
     @classmethod
     def from_bytes(cls, b: bytes):
@@ -18,21 +19,12 @@ class EOCD:
             return int.from_bytes(b[start:end], byteorder='little')
 
         # Find signature...start from smallest possible EOCD (no comment) to largest
-        for i in range(65535, -1, -1):
+        for i in range(len(b)-22, -1, -1):
             if b[i:i+4] == b'PK\x05\x06':
-                comment_len = e(i+20,i+22)
-                comment_last_byte = i+22+comment_len
-                cd_meta = cls(
-                    disk_index=e(i+4,i+6),
-                    cd_start_disk=e(i+6,i+8),
-                    cd_records_on_disk=e(i+8,i+10),
-                    cd_records_total=e(i+10,i+12),
-                    cd_size=e(i+12,i+16),
-                    cd_offset=e(i+16,i+20),
-                    comment_len=comment_len,
-                    comment='' if len(b) < comment_last_byte else b[(i+22):comment_last_byte].decode(),
-                )
-                return cd_meta
+                fixed_data = struct.unpack(r'<4H2LH', b[i+4:i+22])
+                comment_last_byte = i+22+fixed_data[6]
+                comment = '' if len(b) < comment_last_byte else b[i+22:comment_last_byte].decode()
+                return cls(**dict(zip(map(lambda field: field.name, fields(EOCD)), fixed_data + (comment,))))
         return None
 
     def to_bytes(self) -> bytes:
@@ -61,63 +53,47 @@ class CDFileHeader:
     compressed_size: int
     uncompressed_size: int
     filename_len: int
+    extra_field_len: int
+    file_comment_len: int
     disk_of_file_start: int
     internal_file_attr: bytes
     external_file_attr: bytes
     file_header_offset: int
     filename: str
-    extra_field_len: int=0
-    file_comment_len: int=0
-    extra_field: str=''
-    comment: str=''
+    extra_field: str
+    comment: str
 
     @classmethod
     def from_bytes(cls, b: bytes):
-        def e(start: int, end: int):
-            return int.from_bytes(b[start:end], byteorder='little')
-
         # Find signature
         for i in range(0, len(b) - 4):
             if b[i:i+4] == b'PK\x01\x02':
-                filename_len = e(i+28,i+30)
-                extra_field_len = e(i+30,i+32)
-                file_comment_len = e(i+32,i+34)
-                filename_last_byte = i+46+filename_len
-                extra_field_last_byte = filename_last_byte+extra_field_len
-                comment_last_byte = extra_field_last_byte+file_comment_len
-                cd_meta = cls(
-                    made_by_version=e(i+4,i+6),
-                    min_version_needed=e(i+6,i+8),
-                    bit_flags=b[(i+8):(i+10)],
-                    compression_method=e(i+10,i+12),
-                    last_mod_time=e(i+12,i+14),
-                    last_mod_date=e(i+14,i+16),
-                    uncompressed_crc=b[(i+16):(i+20)],
-                    compressed_size=e(i+20,i+24),
-                    uncompressed_size=e(i+24,i+28),
-                    filename_len=filename_len,
-                    extra_field_len=extra_field_len,
-                    file_comment_len=file_comment_len,
-                    disk_of_file_start=e(i+34,i+36),
-                    internal_file_attr=b[(i+36):(i+38)],
-                    external_file_attr=b[(i+38):(i+42)],
-                    file_header_offset=e(i+42,i+46),
-                    filename='' if len(b) < filename_last_byte else b[(i+46):filename_last_byte].decode(),
-                    extra_field='' if len(b) < extra_field_last_byte else b[filename_last_byte:extra_field_last_byte].decode(),
-                    comment='' if len(b) < comment_last_byte else b[extra_field_last_byte:comment_last_byte].decode()
+                fixed_data = struct.unpack(r'<6H4s2L4H2s4sL', b[i+4:i+46])
+                variable_length = fixed_data[9] + fixed_data[10] + fixed_data[11]
+                variable_data = struct.unpack(
+                    f"{fixed_data[9]}s{fixed_data[10]}s{fixed_data[11]}s",
+                    b[i+46:i+46+variable_length],
                 )
-                return cd_meta
+                payload = fixed_data + tuple(map(bytes.decode, variable_data))
+                return cls(**dict(zip(map(lambda field: field.name, fields(CDFileHeader)), payload)))
         return None
 
     @classmethod
     def gen_from_bytes(cls, b: bytes):
         start_byte = 0
         while start_byte < len(b):
-            cd_meta = cls.from_bytes(b[start_byte:])
-            if not cd_meta:
+            signature = b[start_byte:start_byte+4]
+            if signature == b'PK\x05\x06': # Found EOCD
                 break
-            yield start_byte, cd_meta
-            start_byte += 46+cd_meta.filename_len+cd_meta.file_comment_len
+            elif signature == b'PK\x01\x02': # Found CDFileHeader
+                cd_meta = cls.from_bytes(b[start_byte:])
+                if cd_meta:
+                    yield cd_meta
+                    start_byte += 46+cd_meta.filename_len+cd_meta.file_comment_len+cd_meta.extra_field_len
+                else:
+                    start_byte += 1
+            else:
+                start_byte += 1
 
     def to_bytes(self) -> bytes:
         def d(value: int, size: int) -> bytes:
@@ -155,9 +131,9 @@ class FileHeader:
     compressed_size: int
     uncompressed_size: int
     filename_len: int
+    extra_field_len: int
     filename: str
-    extra_field_len: int=0
-    extra_field: str=''
+    extra_field: str
 
     @classmethod
     def from_bytes(cls, b: bytes):
@@ -167,25 +143,14 @@ class FileHeader:
         # Find signature
         for i in range(0, len(b) - 4):
             if b[i:i+4] == b'PK\x03\x04':
-                filename_len = e(i+26,i+28)
-                extra_field_len = e(i+28,i+30)
-                filename_last_byte = i+30+filename_len
-                extra_field_last_byte = filename_last_byte+extra_field_len
-                cd_meta = cls(
-                    min_version_needed=e(i+4,i+6),
-                    bit_flags=b[(i+6):(i+8)],
-                    compression_method=e(i+8,i+10),
-                    last_mod_time=e(i+10,i+12),
-                    last_mod_date=e(i+12,i+14),
-                    uncompressed_crc=b[(i+14):(i+18)],
-                    compressed_size=e(i+18,i+22),
-                    uncompressed_size=e(i+22,i+26),
-                    filename_len=filename_len,
-                    extra_field_len=extra_field_len,
-                    filename='' if len(b) < filename_last_byte else b[(i+30):filename_last_byte].decode(),
-                    extra_field='' if len(b) < extra_field_last_byte else b[filename_last_byte:extra_field_last_byte].decode(),
+                fixed_data = struct.unpack(r'<5H3L2H', b[i+4:i:30])
+                variable_length = fixed_data[8] + fixed_data[9]
+                variable_data = struct.unpack(
+                    f"{fixed_data[8]}s{fixed_data[9]}s",
+                    b[i+30:i+30+variable_length],
                 )
-                return cd_meta
+                payload = fixed_data + tuple(map(bytes.decode, variable_data))
+                return cls(**dict(zip(map(lambda field: field.name, fields(FileHeader)), payload)))
         return None
 
     @classmethod
